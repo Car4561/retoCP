4116f7fe8c98967dbb33ea5b276c59cde931a1d6
Carlos Alfredo Llerena <carlitos_ll31@hotmail.com>
2021-07-19 17:34:02 -0500

    file location fixed

---
+++ EcommerceCineplanet/PartialViews/PremierSlide.swift
@@ -0,0 +1,16 @@
+//
+//  HomeSlide.swift
+//  EcommerceCineplanet
+//
+//  Created by carlos alfredo llerena huayta on 17.07.21.
+//
+
+import UIKit
+
+class PremierSlide: UIView {
+    
+    @IBOutlet weak var image: UIImageView!
+    
+    @IBOutlet weak var title: UILabel!
+    
+}
